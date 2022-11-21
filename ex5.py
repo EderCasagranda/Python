72658a77fa67b953f101570e22bf7d415b13ee91
EderCasagranda <116317172+EderCasagranda@users.noreply.github.com>
2022-11-21 19:53:31 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,2 +1,4 @@
 nome=input('Qual é o seu nome: ')
 print('Olá {}, é um prazer te conhecer' .format(nome))
+
+#Uma outra forma de adicionar o nome digitado na mensagem do print é, usar "{}" dentro da mensagem entre aspas e depois fora da mensagem entre aspas usar ".format(nome)#
